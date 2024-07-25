ecf814ff2bd44bbc341407bb33763c964ddabaf6
Nathaniel Diamond <ndiamond39@gmail.com>
2024-07-25 12:22:17 -0400

    feat(Education): Links to Education

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         <div className="fixed inset-0 z-10 w-screen p-2">
           <div className="flex flex-row min-h-full items-stretch justify-center p-4 text-center sm:p-0">
             <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl sm:p-6 ">
-              <iframe className="w-full h-full" src="/resume.pdf" /> 
+              <iframe className="w-full h-full border border-black" src="/resume.pdf" /> 
             </DialogPanel>
           </div>
         </div>
@@ -35,7 +35,7 @@ export default function Home() {
             </div>
             <img className="w-10 h-auto group-hover:animate-bounce" src="icon.png"/>
           </div>
-          <div className={`grow-[10] flex flex-row gap-2 items-start`}>
+          <div className={`flex flex-row gap-2 items-start`}>
             <Button className="group" color={secondaryColor} onPress={() => {setResumeModalVisible(true)}}>
               <DocumentTextIcon aria-hidden="true" className="group-hover:animate-bounce -ml-0.5 h-5 w-5" />
             </Button>
@@ -47,6 +47,16 @@ export default function Home() {
             </Button>
             <Button className="group" color={secondaryColor} onPress={() => window.open("https://x.com/N8Diamond")}>
               <X className="group-hover:animate-bounce"/>
+            </Button>
+          </div>
+          <div className="flex flex-row items-stretch gap-4">
+            <Button className="group" color={secondaryColor} onPress={() => window.open("https://en.wikipedia.org/wiki/Cornell_University_College_of_Engineering")}>
+              <div className="py-4">
+                <img className="group-hover:animate-bounce" width={100} src="cornell.png"/>
+              </div>
+            </Button>
+            <Button className="group" color={secondaryColor} onPress={() => window.open("https://www.machonyaakov.org/")}>
+              <img className="group-hover:animate-bounce" width={150} src="machon.gif" />
             </Button>
           </div>
           <div className="grow flex flex-row justify-end items-end gap-2">
