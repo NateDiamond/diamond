ab88d4e29402f074e7f3d3fec402f766b9d56066
Nathaniel Diamond <ndiamond39@gmail.com>
2024-07-29 16:45:59 -0400

    fix(Mobile): More Responsive

--- src/app/components/text.tsx
+++ src/app/components/text.tsx
@@ -1,4 +1,8 @@
+import { BRIGHTNESS } from "./color/brightness";
+import { getColor } from "./color/colors";
+
 type LabelParams = {name: string};
+type TextParams = {children: string, className?: string, color: string, brightness: BRIGHTNESS, center?: boolean, large?: boolean};
 
 export function Label({name}: LabelParams) {
   return (
@@ -6,4 +10,12 @@ export function Label({name}: LabelParams) {
       <span className="text-slate-900 dark:text-slate-200">{name}</span>
     </label>
   );
+}
+
+export function Text({children, className, color, brightness, center = false, large = false}: TextParams) {
+  return (
+    <div className={`${className} ${getColor("text", color, brightness, true, false)} text-center ${center ? '': 'sm:text-start'} font-light font-josefin ${large ? 'text-2xl': 'text-lg'}`}>
+      {children}
+    </div>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import { Github, LinkedIn, Resume, X, Diamond } from "./components/icons";
 import { Toggle } from "./components/toggle";
 import { BRIGHTNESS } from "./components/color/brightness";
 import { Optional } from "./components/types";
+import { Text } from "./components/text";
 
 function getRandom(n: number) {
   return Math.floor(Math.random() * n);
@@ -49,77 +50,94 @@ export default function Home() {
         </div>
       </Dialog>
       <div className={`flex min-h-screen flex-col items-center justify-between p-4 bg-gradient-to-br ${getColor("from", primaryColor, brightness)} ${getColor("to", secondaryColor, brightness)} ${resumeModalVisible ? 'opacity-50' : ''}`}>
-        <div className={`grow w-full max-w-7xl font-mono text-sm flex flex-col p-8 gap-4 rounded-lg shadow-2xl border ${getColor("border", secondaryColor, brightness, true, false)} bg-gradient-to-br ${getColor("from", secondaryColor, brightness)} ${getColor("to", primaryColor, brightness)}`}>
-          <Button description="Hello, world!" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} pressable={false}>
-            <>
-              <Diamond className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} />
-              <div className={`${getColor("text", secondaryColor, brightness, true, false)} text-center font-light font-josefin text-2xl`}>
-                Nathaniel Diamond
-              </div>
-              <Diamond className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} />
-            </>
-          </Button>
-          <div className={`flex flex-row gap-2 items-start`}>
-            <Button description="Nate's Resume" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => {setResumeModalVisible(true)}}>
-              <Resume className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness}/>
-            </Button>
-            <Button description="See my work on Github" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.github.com/NateDiamond")}>
-              <Github className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness}/>
-            </Button> 
-            <Button description="Connect with me on LinkedIn" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.linkedin.com/in/nathaniel-diamond-225241127")}>
-              <LinkedIn className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness}/>
-            </Button>
-            <Button description="Follow me on X" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness}  onPress={() => window.open("https://x.com/N8Diamond")}>
-              <X className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness}/>
+        <div className={`w-full max-w-7xl font-mono text-sm flex flex-col p-8 gap-4 rounded-lg shadow-2xl border ${getColor("border", secondaryColor, brightness, true, false)} bg-gradient-to-br ${getColor("from", secondaryColor, brightness)} ${getColor("to", primaryColor, brightness)}`}>
+          <div className="flex flex-row">
+            <Button description="Hello, world!" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} pressable={false}>
+              <>
+                <Diamond className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} />
+                <Text color={secondaryColor} brightness={brightness} center={true} large={true}>
+                  Nathaniel Diamond
+                </Text>
+                <Diamond className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} />
+              </>
             </Button>
           </div>
-          <div className="grid grid-cols-2 gap-4">
-            <Button description="Where I went to school" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://en.wikipedia.org/wiki/Cornell_University_College_of_Engineering")}>
-              <div className="py-4">
-                <img className="group-hover:animate-bounce" width={100} src="cornell.png"/>
-              </div>
-            </Button>
-            <Button description="Also where I went to school" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.machonyaakov.org")}>
-              <img className="group-hover:animate-bounce" width={150} src="machon.gif" />
-            </Button>
+          <div>
+            <Text className="pb-2" color={secondaryColor} brightness={brightness}>
+              Socials
+            </Text>
+            <div className={`flex flex-row flex-wrap gap-2 items-start`}>
+              <Button description="Nate's Resume" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => {setResumeModalVisible(true)}}>
+                <Resume className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness}/>
+              </Button>
+              <Button description="See my work on Github" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.github.com/NateDiamond")}>
+                <Github className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness}/>
+              </Button> 
+              <Button description="Connect with me on LinkedIn" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.linkedin.com/in/nathaniel-diamond-225241127")}>
+                <LinkedIn className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness}/>
+              </Button>
+              <Button description="Follow me on X" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness}  onPress={() => window.open("https://x.com/N8Diamond")}>
+                <X className="group-hover:animate-bounce" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness}/>
+              </Button>
+            </div>
           </div>
-          <div className="grid grid-cols-2 gap-4">
-            <Button description="Mint Doginals with my API" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.doginalsbot.io")}>
-              <div className="py-4">
-                <img className="group-hover:animate-bounce" width={100} src="doginalsBotLogo.webp"/>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-10">
+            <div>
+              <Text className="pb-2" color={secondaryColor} brightness={brightness}>
+                Education
+              </Text>
+              <div className="grid grid-cols-2 gap-4">
+                <Button description="Where I went to school" className="group h-32" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://en.wikipedia.org/wiki/Cornell_University_College_of_Engineering")}>
+                  <div className="py-4">
+                    <img className="group-hover:animate-bounce" width={80} src="cornell.png"/>
+                  </div>
+                </Button>
+                <Button description="Also where I went to school" className="group h-32" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.machonyaakov.org")}>
+                  <img className="group-hover:animate-bounce" width={120} src="machon.gif" />
+                </Button>
               </div>
-            </Button>
-            <Button description="Mint, trade, and admire crypto and NFTs with our new app" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.wildtangz.com")}>
-              <img className="group-hover:animate-bounce" width={100} src="wildtangz.png" />
-            </Button>
-          </div>
-          <div className={`flex flex-col sm:flex-row gap-2 items-stretch`}>
+            </div>  
+            <div>
+              <Text className="pb-2" color={secondaryColor} brightness={brightness}>
+                Projects
+              </Text>
+              <div className="grid grid-cols-2 gap-4">
+                <Button description="Mint Doginals with my API" className="group h-32" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.doginalsbot.io")}>
+                  <div className="py-4">
+                    <img className="group-hover:animate-bounce" width={70} src="doginalsBotLogo.webp"/>
+                  </div>
+                </Button>
+                <Button description="Mint, trade, and admire crypto and NFTs with our new app" className="group h-32" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.wildtangz.com")}>
+                  <img className="group-hover:animate-bounce" width={70} src="wildtangz.png" />
+                </Button>
+              </div>
+            </div>                
+          </div> 
+          <div>
+          <Text className="pb-2" color={secondaryColor} brightness={brightness}>
+            Skills
+          </Text>
+          <div className={`flex flex-row flex-wrap gap-2 items-stretch`}>
             <Button description="I'm skilled in React and React Native" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://reactnative.dev")}>
-              <div className="pt-2 sm:py-0">
-                <img className="group-hover:animate-bounce" width={100} src="react.webp" />
-              </div>
+              <img className="group-hover:animate-bounce" width={50} src="react.webp" />
             </Button>
             <Button description="I'm skilled in Javascript" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.javascript.com")}>
-              <div className="pt-2 sm:py-0">
-                <img className="group-hover:animate-bounce" width={100} src="js.webp" />
-              </div>
+              <img className="group-hover:animate-bounce" width={50} src="js.webp" />
             </Button>
             <Button description="I'm skilled in Tailwind CSS and Native Wind" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://tailwindcss.com")}>
-              <div className="py-2 sm:py-0">
-                <img className="group-hover:animate-bounce" width={100} src="tailwind.png" />
-              </div>
+              <img className="group-hover:animate-bounce" width={50} src="tailwind.png" />
             </Button>
             <Button description="I'm skilled in Amazon Web Services" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://aws.amazon.com")}>
-              <div className="pt-2 sm:py-0">
-                <img className="group-hover:animate-bounce" width={100} src="aws.png" />
-              </div>
+              <img className="group-hover:animate-bounce" width={50} src="aws.png" />
             </Button>
-              <Button description="I'm skilled in Python" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.python.org")}>
-                <div className="pt-2">
-                  <img className="group-hover:animate-bounce" width={100} src="python.png" />
-                </div>     
-              </Button>
+            <Button description="I'm skilled in Python" className="group" color={primaryColor} secondaryColor={secondaryColor} brightness={brightness} onPress={() => window.open("https://www.python.org")}>
+              <img className="group-hover:animate-bounce" width={50} src="python.png" />
+            </Button>
           </div>
+          </div>         
+          <Text className="pt-4" color={secondaryColor} brightness={brightness}>
+            Settings
+          </Text>
           <div className="flex flex-col sm:flex-row justify-between items-stretch sm:items-center">
             <div className="flex flex-row justify-center">
               <Toggle
